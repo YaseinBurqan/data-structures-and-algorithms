5f6b13f366a594ec249f551327e46b50bcb0ba77
Yasein Burqan <mr.yasein@gmail.com>
2022-06-06 23:38:42 +0300

    data-structures-and-algorithms

--- javascript/array-binary-search/array-binary-search.js
+++ javascript/array-binary-search/array-binary-search.js
@@ -1,6 +1,26 @@
-'use strict';
+/* eslint-disable quotes */
+/* eslint-disable new-cap */
+"use strict";
 
-function BinarySearch(arr, value) {
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let value = 10;
+
+let BinarySearch = function (arr, value, start, end) {
+  if (start > end) return false;
+  let mid = Math.floor((start + end) / 2);
+
+  if (arr[mid] === value) {
+    return true;
+  }
+  if (arr[mid] > value) {
+    return BinarySearch(arr, value, start, mid - 1);
+  } else {
+    return BinarySearch(arr, value, mid + 1, end);
+  }
+};
+
+if (BinarySearch(arr, value, 0, arr.length - 1)) {
+  console.log("Element found : " + value);
+} else {
+  console.log("Element not found");
 }
-// eslint-disable-next-line new-cap
-BinarySearch();
