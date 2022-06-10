e398d4226ee6f67ff9c2b060e6636ea2cf2d5680
Yasein Burqan <mr.yasein@gmail.com>
2022-06-11 02:10:13 +0300

    data-structures-and-algorithms

--- javascript/array-binary-search/README.md
+++ javascript/array-binary-search/README.md
@@ -12,8 +12,11 @@ Check the Resources section for details
 ## Whiteboard Process
 <!-- Embedded whiteboard image -->
 
-
 ## Approach & Efficiency
 <!-- What approach did you take? Discuss Why. What is the Big O space/time for this approach? -->
+
+    I used loop approach (while), but in this logic there is no need to loop through all array's elements and because the array is sorted this helped with my logic. So there was O(log(n)) as time complexity and O(1) as space complexity.
+
+![array-binary-search](./array-binary-search.jpg)
 
 [Code](./array-binary-search.js)
--- javascript/array-binary-search/array-binary-search.js
+++ javascript/array-binary-search/array-binary-search.js
@@ -5,22 +5,22 @@
 let arr = [4, 8, 15, 16, 23, 42];
 let value = 15;
 
-let BinarySearch = function (arr, value, start, end) {
-  if (start > end) return false;
-  let mid = Math.floor((start + end) / 2);
+function arrayBinarySearch(arr, value) {
+  let minValue = 0;
+  let maxValue = arr.length - 1;
 
-  if (arr[mid] === value) {
-    return true;
+  while (minValue <= maxValue) {
+    let midValue = Math.floor((minValue + maxValue) / 2);
+    let middleIndex = arr[midValue];
+    if (middleIndex === value) {
+      return midValue;
+    }
+    if (middleIndex > value) {
+      maxValue = midValue - 1;
+    } else {
+      minValue = midValue + 1;
+    }
   }
-  if (arr[mid] > value) {
-    return BinarySearch(arr, value, start, mid - 1);
-  } else {
-    return BinarySearch(arr, value, mid + 1, end);
-  }
-};
-
-if (BinarySearch(arr, value, 0, arr.length - 1)) {
-  console.log("Element found : " + value);
-} else {
-  console.log("Element not found : -1");
+  return -1;
 }
+console.log(arrayBinarySearch(arr, value));
