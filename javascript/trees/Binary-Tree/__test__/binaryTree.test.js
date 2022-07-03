b93a10d6631be1dc971817866d56a3aff9ff0cbc
Yasein Burqan <mr.yasein@gmail.com>
2022-07-04 00:07:43 +0300

    data-structures-and-algorithms

--- javascript/trees/Binary-Tree/__test__/binaryTree.test.js
+++ javascript/trees/Binary-Tree/__test__/binaryTree.test.js
@@ -1,7 +1,7 @@
 /* eslint-disable quotes */
 "use strict";
 const Node = require("../node");
-const BinaryTree = require("../src/binaryTree");
+const BinaryTree = require("../src/BinaryTree");
 const BinarySearchTree = require("../src/BinarySearchTree");
 
 let tree = null;
@@ -29,40 +29,28 @@ describe("Binary Tree", () => {
 
     tree = new BinaryTree(one);
   });
+
   test("constructor", () => {
     const tree = new BinaryTree();
     expect(tree.root).toBeNull();
-    expect(tree.root.value).toEqual(1);
   });
 
   test("preOrder", () => {
-    tree = new BinaryTree();
-    const preOrder = tree.PreOrder();
-    expect(preOrder).toEqual([1, 2, 6, 7, 8, 9, 3, 4, 5]);
+    let expectedOutput = [1, 2, 6, 7, 8, 9, 3, 4, 5];
+    let preOrder = tree.preOrder();
+    expect(preOrder).toEqual(expectedOutput);
   });
 
   test("inOrder", () => {
-    const inOrder = tree.InOrder();
-    expect(inOrder).toEqual([6, 8, 7, 9, 2, 1, 4, 3, 5]);
+    let expectedOutput = [6, 8, 7, 9, 2, 1, 4, 3, 5];
+    let inOrder = tree.inOrder();
+    expect(inOrder).toEqual(expectedOutput);
   });
 
   test("postOrder", () => {
-    const postOrder = tree.PostOrder();
-    expect(postOrder).toEqual([8, 9, 7, 6, 2, 4, 5, 3, 1]);
-  });
-
-  test("Binary Search", () => {
-    const BST = new BinaryTreeSearch();
-    BST.add(10);
-    BST.add(11);
-    BST.add(5);
-    BST.add(13);
-    BST.add(13);
-    BST.add(6);
-    BST.add(15);
-    expect(BST.contains(6)).toBeTruthy();
-    expect(BST.contains(15)).toBeTruthy();
-    expect(BST.contains(20)).toBeFalsy();
+    let expectedOutput = [8, 9, 7, 6, 2, 4, 5, 3, 1];
+    let postOrder = tree.postOrder();
+    expect(postOrder).toEqual(expectedOutput);
   });
 
   test("Can successfully instantiate an empty tree", () => {
@@ -73,17 +61,52 @@ describe("Binary Tree", () => {
   test("Can successfully instantiate a tree with a single root node", () => {
     let node = new Node(1);
     const tree = new BinaryTree(node);
-    expect(tree.root.value).toEqual(1);
+    expect(tree.root.data).toEqual(1);
   });
 
-  test("Can successfully add a left child and right child to a tree", () => {
-    const BST = new BinarySearchTree();
-    BST.add(10);
-    BST.add(11);
-    BST.add(5);
+  test("For a Binary Search Tree, can successfully add a left child and right child properly to a node", () => {
+    const n1 = new Node(1);
+    const n2 = new Node(2);
+    const n3 = new Node(3);
+    let tree = new BinaryTree(n1);
+    n1.left = n2;
+    n1.right = n3;
+    expect(tree.root.data).toBe(1);
+    expect(tree.root.left.data).toBe(2);
+    expect(tree.root.right.data).toBe(3);
+  });
 
-    expect(BST.root.value).toEqual(10);
-    expect(BST.root.left.value).toEqual(5);
-    expect(BST.root.right.value).toEqual(11);
+  test("Can successfully return a collection from a preOrder traversal", () => {
+    let arr = [1, 2, 6, 7, 8, 9, 3, 4, 5];
+    expect(tree.preOrder()).toEqual(arr);
+  });
+
+  test("Can successfully return a collection from an inOrder traversal", () => {
+    let arr = [6, 8, 7, 9, 2, 1, 4, 3, 5];
+    expect(tree.inOrder()).toEqual(arr);
+  });
+
+  test("Can successfully return a collection from a postOrder traversal", () => {
+    let arr = [8, 9, 7, 6, 2, 4, 5, 3, 1];
+    expect(tree.postOrder()).toEqual(arr);
+  });
+
+  test("false or true for the contains method, given an existing or non-existing node value", () => {
+    const n1 = new Node(1);
+    const n2 = new Node(2);
+    const n3 = new Node(3);
+    const n4 = new Node(4);
+    const n5 = new Node(5);
+    const n6 = new Node(7);
+
+    let tree = new BinarySearchTree(n1);
+
+    n1.left = n2;
+    n1.right = n3;
+    n2.left = n4;
+    n2.right = n5;
+    n3.left = n6;
+
+    expect(tree.contains(6)).toBe(false);
   });
 });
