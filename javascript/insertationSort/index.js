79d99c9013794e90af9895863696b3ef5efc3d11
Yasein Burqan <mr.yasein@gmail.com>
2022-08-15 14:00:00 +0300

    qick sort

--- javascript/insertationSort/index.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable quotes */
-"use strict";
-
-const express = require("express");
-const app = express();
-
-const InsertionSort = require("./src/insertion.js");
-
-app.use(InsertionSort);
