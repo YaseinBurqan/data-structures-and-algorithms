c7a10c8555ecaeba4d042f1c1baf8003793135bd
Yasein Burqan <mr.yasein@gmail.com>
2022-08-25 14:12:45 +0300

    Intersection

--- javascript/hash-table/hashTable.js
+++ javascript/hash-table/hashTable.js
@@ -1,4 +1,4 @@
-// const LinkedList = require("../linked-list/linkedList");
+const LinkedList = require("../linked-list/linkedList");
 class HashTable {
   constructor(size) {
     this.table = new Array(size);
@@ -14,36 +14,25 @@ class HashTable {
   }
 
   set(key, value) {
-    const index = this.hash(key);
-    const bucket = this.table[index];
-    if (!bucket) {
-      this.table[index] = [[key, value]];
-    } else {
-      const sameKeyItem = bucket.find((item) => item[0] === key);
-      if (sameKeyItem) {
-        sameKeyItem[1] = value;
-      } else {
-        bucket.push([key, value]);
-      }
+    let index = this.hash(key);
+    if (!this.table[index]) {
+      this.table[index] = new LinkedList();
     }
+    this.table[index].append({ [key]: value });
   }
 
   get(key) {
-    const index = this.hash(key);
-    const bucket = this.table[index];
-    if (bucket) {
-      const sameKeyItem = bucket.find((item) => item[0] === key);
-      if (sameKeyItem) {
-        return sameKeyItem[1];
-      }
+    let index = this.hash(key);
+    if (!this.table[index]) {
+      return null;
     }
-    return undefined;
+    return this.table[index].values();
   }
 
   remove(key) {
     let index = this.hash(key);
     const bucket = this.table[index];
-    if (bucket) {
+    if (bucket && bucket.length) {
       const sameKeyItem = bucket.find((item) => item[0] === key);
       if (sameKeyItem) {
         bucket.splice(bucket.indexOf(sameKeyItem), 1);
@@ -51,11 +40,34 @@ class HashTable {
     }
   }
 
-  display() {
+  contains(key) {
+    const hash = this.hash(key);
+    if (this.table[hash]) {
+      let currentNode = this.table[hash].head;
+      while (currentNode) {
+        if (currentNode.value[key]) {
+          return true;
+        }
+        currentNode = currentNode.next;
+      }
+    } else {
+      return false;
+    }
+  }
+
+  keys() {
+    let keys = [];
     for (let i = 0; i < this.table.length; i++) {
       if (this.table[i]) {
-        console.log(i, this.table[i]);
+        keys.push(this.table[i].keys());
       }
+    }
+    return keys;
+  }
+
+  display() {
+    for (let i = 0; i < this.table.length; i++) {
+      this.table[i];
     }
   }
 }
